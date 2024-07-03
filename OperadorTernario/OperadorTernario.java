c5dc2da5514631acb1c3e13ec56cd26478f3f6a6
JLuisGit <JoseLuisOsorioGuzman11@gmail.com>
2024-07-03 18:36:23 -0500

    Operadores Ternarios

---
+++ OperadorTernario/OperadorTernario.java
@@ -0,0 +1,20 @@
+public class OperadorTernario {
+
+    public static void main(String[] args) {
+
+        // Operador Ternario
+        // IFTTT
+        // ?
+        // expresion1 ? expresion2 : expresion3
+        // Si expresion 1 = true entonces expresion2
+        // Si expresion 2 = false entonces expresion3
+
+        String mensaje = 10 > 9 ? "Es mayor" : "Es menor";
+
+        int enteros = true ? 1 : 0;
+
+        System.out.println(mensaje);
+        System.out.println(enteros);
+
+    }
+}
